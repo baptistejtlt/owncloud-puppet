5699bf5c4bc761ed56435f829f87ddeeda94aae1
Baptiste Jentellet <baptiste.jentellet@etu.univ-lyon1.fr>
2016-06-08 10:22:11 +0200

    Adding operating system for Debian and RedHat family.

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,8 +6,13 @@
 #==============================================================================
 
 class owncloud::install {
+
+    case $::operatingsystem {
+
       'Debian': {
+
         include ::apt
+
         apt::source { 'owncloud':
           location => "http://download.opensuse.org/repositories/isv:/ownCloud:/community/Debian_${::operatingsystemmajrelease}.0/",
           release  => ' ',
@@ -18,7 +23,61 @@ class owncloud::install {
           },
           before   => Package[$::owncloud::package_name],
         }
-}
-  package { $::owncloud::package_name:
-    ensure => present,
-}
+
+      }
+
+      'Ubuntu': {
+        include ::apt
+
+        if $::lsbdistcodename == 'precise' {
+          apt::ppa { 'ppa:ondrej/php5-oldstable':
+            before => Package[$::owncloud::package_name],
+          }
+        }
+
+        apt::source { 'owncloud':
+          location => "http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_${::operatingsystemrelease}/",
+          release  => ' ',
+          repos    => '/',
+          key      => {
+            id     => 'F9EA4996747310AE79474F44977C43A8BA684223',
+            source => "http://download.opensuse.org/repositories/isv:/ownCloud:/community/xUbuntu_${::operatingsystemrelease}/Release.key",
+          },
+          before   => Package[$::owncloud::package_name],
+        }
+      }
+
+      'CentOS': {
+        include ::epel
+
+        if $::operatingsystemmajrelease == '6' {
+          include ::remi
+        }
+
+        yumrepo { 'isv:ownCloud:community':
+          name     => 'isv_ownCloud_community',
+          descr    => "ownCloud Server Version stable (CentOS_${::operatingsystemmajrelease})",
+          baseurl  => "https://download.owncloud.org/download/repositories/stable/CentOS_${::operatingsystemmajrelease}/",
+          gpgcheck => 1,
+          gpgkey   => "https://download.owncloud.org/download/repositories/stable/CentOS_${::operatingsystemmajrelease}/repodata/repomd.xml.key",
+          enabled  => 1,
+          before   => Package[$::owncloud::package_name],
+        }
+      }
+
+      'Fedora': {
+        yumrepo { 'isv:ownCloud:community':
+          name     => 'isv_ownCloud_community',
+          descr    => "Latest stable community release of ownCloud (Fedora_${::operatingsystemmajrelease})",
+          baseurl  => "http://download.opensuse.org/repositories/isv:/ownCloud:/community/Fedora_${::operatingsystemmajrelease}/",
+          gpgcheck => 1,
+          gpgkey   => "http://download.opensuse.org/repositories/isv:/ownCloud:/community/Fedora_${::operatingsystemmajrelease}/repodata/repomd.xml.key",
+          enabled  => 1,
+          before   => Package[$::owncloud::package_name],
+        }
+      }
+
+
+
+    }
+}  
